22563561b7d5857d3796fb5578f36d3c9da35abc
James Ha <hajamesjw@gmail.com>
2018-11-02 02:12:21 -0400

    isUnique: Returns true when characters are unique.

--- src/main/java/com/jamesha/javaexercises/javaexercises/ArraysAndStrings.java
+++ src/main/java/com/jamesha/javaexercises/javaexercises/ArraysAndStrings.java
@@ -2,4 +2,23 @@ package com.jamesha.javaexercises.javaexercises;
 
 public class ArraysAndStrings {
 
+	/*
+	 * p.90 1.1
+	 * Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+	 */
+	public static boolean isUnique(String string) {
+		return true;
+	}
+	
+	/*
+	 * p.90 1.2
+	 * Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+	 */
+	
+	
+	/*
+	 * p.90 1.3
+	 * URLify: Write a method to replace all spaces in a string with '%20: You may assume that the string has sufficient space at the end to hold the additional characters, 
+	 * and that you are given the "true" length of the string. (Note: If implementing in Java, please use a character array so that you can perform this operation in place.)
+	 */
 }
--- src/test/java/com/jamesha/javaexercises/javaexercises/TestArraysAndStrings.java
+++ src/test/java/com/jamesha/javaexercises/javaexercises/TestArraysAndStrings.java
@@ -1,5 +1,13 @@
 package com.jamesha.javaexercises.javaexercises;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class TestArraysAndStrings {
 	
+	@Test
+	public void testIsUniqueReturnsTrueWhenAllCharactersAreUnique() {
+		assertTrue(ArraysAndStrings.isUnique("abcdefghijklmnop"));
+	}
 }
